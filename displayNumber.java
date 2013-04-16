80c122d5371bbfa1474ddee1111a7a06c38ab959
sacprogrammer <abenitez@uci.edu>
2013-04-15 21:20:23 -0700

    Create displayNumber.java

---
+++ displayNumber.java
@@ -0,0 +1,12 @@
+
+public class displayNumber {
+
+  public static void main(String[] args) {
+
+  	randomizer randomNum = new randomizer();
+  	
+  	System.out.println(randomNum.theNumber());
+  	
+  	
+  }
+}
